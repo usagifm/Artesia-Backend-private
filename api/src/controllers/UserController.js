30b1a36cdc0b06758101376c96c5a2fefce78577
Intern 01 <rashad.fadilah@ibm-jti.com>
2022-01-17 11:33:50 +0700

    Update UserController.js

--- api/src/controllers/UserController.js
+++ api/src/controllers/UserController.js
@@ -18,9 +18,9 @@ const UserController = {
         const discovered_artefacts = await UserArtefact.count({
         where: {UserId : req.user.user.id}});
 
-            user.artefact_counts = discovered_artefacts;
+            user['artefact_counts'] = discovered_artefacts;
                 
-            return res.status(200).send({user, artefact_counts: discovered_artefacts})
+            return res.status(200).send(user)
 
             } catch (error) {
                 console.log(error)
